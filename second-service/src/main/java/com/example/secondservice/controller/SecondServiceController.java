9ec580ccca672b5f6927a70f64b36c77ac81c11d
eogh6428@naver.com <destiny1017@nate.com>
2024-06-15 13:19:32 +0900

    first-service, second-service module init

---
+++ second-service/src/main/java/com/example/secondservice/controller/SecondServiceController.java
@@ -0,0 +1,15 @@
+package com.example.secondservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/second-service")
+public class SecondServiceController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello second-service!!";
+    }
+}
