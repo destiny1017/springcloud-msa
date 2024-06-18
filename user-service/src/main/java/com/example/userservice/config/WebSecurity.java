2d05affd146e994720e0c1e22824ec60029d638e
eogh6428@naver.com <destiny1017@nate.com>
2024-06-18 20:50:24 +0900

    user-service spring security 적용

---
+++ user-service/src/main/java/com/example/userservice/config/WebSecurity.java
@@ -0,0 +1,24 @@
+package com.example.userservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/user-service/**").permitAll()
+                );
+
+        return http.build();
+    }
+}
--- user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.userservice.dto.UserDto;
 import com.example.userservice.jpa.UserRepository;
 import com.example.userservice.vo.ResponseUser;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -13,10 +14,11 @@ import java.util.UUID;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     @Override
     public ResponseUser createUser(UserDto userDto) {
-        userDto.setPwd("encrypted pwd" + userDto.getPwd());
+        userDto.setPwd(passwordEncoder.encode(userDto.getPwd()));
         userDto.setUserId(UUID.randomUUID().toString());
         userRepository.save(UserDto.toEntity(userDto));
         return UserDto.toResponse(userDto);
