e05f91c5ca7e79b47f343db8c2777e844133593b
KDH <eogh6428@naver.com>
2024-07-08 21:27:30 +0900

    config-server configuration

--- config-service/src/main/java/org/example/ConfigServiceApplication.java
+++ config-service/src/main/java/org/example/ConfigServiceApplication.java
@@ -1,11 +1,14 @@
 package org.example;
 
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServiceApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(ConfigServiceApplication.class, args);
     }
 }
